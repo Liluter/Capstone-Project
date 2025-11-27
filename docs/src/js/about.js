25793148ea077c638c39238e7e445d1aafa04150
Liluter <karolawdziewicz@gmail.com>
2025-11-27 18:39:27 +0100

    Hack with docs

--- docs/src/js/about.js
+++
@@ -1,7 +0,0 @@
-import {Cart} from "./cart.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-	const cartCounterElement = document.querySelector("#cartCounter");
-	const myCart = new Cart(cartCounterElement);
-	myCart.cartInit();
-});
